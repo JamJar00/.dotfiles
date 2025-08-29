9541c3c5255075fa12f88587bee39df9ca8039c8
Jamie Read <jamie.read@and.digital>
2025-08-29 14:31:51 +0100

    refactor: ready for new install

--- install.py
+++ install.py
@@ -16,7 +16,35 @@ parser.add_argument("--with-wacom", action="store_true")
 
 args = envbot.init_from_parser(parser)
 
-# Dotfiles
+@envbot.step("Prompt install")
+def install_prompt():
+    if envbot.util.is_command_installed("prompt"):
+        raise envbot.StepSkipped()
+
+    envbot.lowlevel.git.git_clone("git@github.com:JamJar00/prompt.git", "~/Projects/prompt")
+    envbot.shell("cd ~/Projects/prompt && cargo build")
+    envbot.ensure_file_contains_text("~/.config/fish/config.fish.local", "fish_add_path ~/Projects/prompt/target/debug")
+
+
+@envbot.step(".NET tool install", "{0}")
+def install_dotnet_tool(name):
+    if envbot.util.is_command_installed(name):
+        raise envbot.StepSkipped()
+
+    envbot.shell("dotnet tool install --global " + name)
+
+
+@envbot.step("Install Rust Component", "{0}")
+def install_rust_component(name):
+    if envbot.util.is_command_installed(name):
+        raise envbot.StepSkipped()
+
+    envbot.shell("rustup component add " + name)
+
+
+#
+# Generic stuff
+#
 envbot.symlink("bashrc", "~/.bashrc")
 envbot.symlink("gitconfig", "~/.gitconfig")
 envbot.symlink("vimrc", "~/.vimrc")
@@ -25,45 +53,56 @@ envbot.symlink("config.fish", "~/.config/fish/config.fish")
 
 if envbot.platform == "Darwin":
     envbot.symlink("karabiner", "~/.config/karabiner")
-else:
-    envbot.copy("./windows-terminal.json", "/mnt/c/Users/jamie/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json", overwrite=True)
-
-
-# Packages
-if envbot.platform == "Darwin":
-    # dotnet-sdk omitted as the Homebrew install doesn't set it up right for csharp-ls
-    envbot.install("bash", "bash-completion@2", "caffeine", "docker", "fish", "font-hack-nerd-font", "gnupg", "helm", "iterm2", "jq", "karabiner-elements", "kubectx", "kubernetes-cli", "mcfly", "minikube", "neovim",  "openvpn-connect", "pritunl", "ripgrep", "rust-analyzer", "shellcheck", "terraform-ls", "tflint", "tfsec", "watch")
+    envbot.install("bash", "bash-completion@2", "caffeine", "docker", "fish", "font-hack-nerd-font", "gnupg", "iterm2", "jq", "karabiner-elements", "neovim", "ripgrep", "watch")
 
     if args.with_wacom:
         envbot.install("wacom-tablet")
 
+    # Specify the preferences directory
+    envbot.lowlevel.defaults.write("com.googlecode.iterm2.plist", "PrefsCustomFolder", envbot.cwd + "/iterm2")
+    # Tell iTerm2 to use the custom preferences in the directory
+    envbot.lowlevel.defaults.write("com.googlecode.iterm2.plist", "LoadPrefsFromCustomFolder", True)
+
+if envbot.platform == "Windows":
+    envbot.copy("./windows-terminal.json", "C:/Users/jamie/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json", overwrite=True)
+
+    envbot.add_package_repositories("extras", "nerd-fonts", package_manager="scoop")
+    envbot.install("win32yank", "CascadiaCode-NF-Mono", package_manager="scoop")
+
+if envbot.platform == "Linux":
+    envbot.add_package_repositories("ppa:fish-shell/release-3", "ppa:neovim-ppa/unstable")
+    envbot.install("fish", "neovim")
+
+envbot.install("pynvim", package_manager="pip")
+envbot.shell("nvim --headless +PlugInstall +qall")
+
+install_prompt()
+
+envbot.chsh("fish")
+
+#
+# Machine specific steps
+#
+if envbot.hostname == "FXJXWHJ0W0.local":
+    # dotnet-sdk omitted as the Homebrew install doesn't set it up right for csharp-ls
+    envbot.install("helm", "kubectx", "kubernetes-cli", "minikube", "openvpn-connect", "pritunl", "rust-analyzer", "shellcheck", "terraform-ls", "tflint", "tfsec")
+
     envbot.packs.terraform.install_version("1.3.1")
-else:
+
+    install_dotnet_tool("csharp-ls")
+    install_rust_component("rust-analyzer")
+
+    envbot.install_package_manager("pipx")
+    envbot.install("poetry", "pyright", package_manager="pipx")
+
+elif envbot.hostname == "FEATHERS" or envbot.hostname == "FLUFFLES":
     envbot.packs.node.install_version("22.12.0")
 
     envbot.add_package_repositories("ppa:fish-shell/release-3", "ppa:neovim-ppa/unstable")
     envbot.add_package_repositories("extras", "nerd-fonts", package_manager="scoop")
 
-    envbot.install("dotnet-sdk-8.0", "fish", "neovim")
-    envbot.install("7zip", "coretemp", "cpu-z", "screentogif", "rclone", "win32yank", "yt-dlp", "CascadiaCode-NF-Mono", package_manager="scoop")
-
-    @envbot.step("Install Mcfly")
-    def install_mcfly():
-        if envbot.util.is_command_installed("mcfly"):
-            raise envbot.StepSkipped()
-
-        envbot.shell("curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly --to ~/.bin")
-
-    install_mcfly()
-
-    @envbot.step("Install Rust Analyzer")
-    def install_rust_analyzer():
-        envbot.make_directories("~/.local/bin")
-        envbot.shell("curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer")
-        st = os.stat(os.path.expanduser("~/.local/bin"))
-        envbot.chmod("~/.local/bin/rust-analyzer", st.st_mode | stat.S_IEXEC)
-
-    install_rust_analyzer()
+    envbot.install("dotnet-sdk-8.0")
+    envbot.install("7zip", "coretemp", "cpu-z", "screentogif", "rclone", "yt-dlp", package_manager="scoop")
 
     @envbot.step("Install Docker")
     def install_docker():
@@ -76,42 +115,11 @@ else:
 
     install_docker()
 
+    install_dotnet_tool("csharp-ls")
+    install_rust_component("rust-analyzer")
 
-@envbot.step(".NET tool install", "{0}")
-def install_dotnet_tool(name):
-    if envbot.util.is_command_installed(name):
-        raise envbot.StepSkipped()
-
-    envbot.shell("dotnet tool install --global " + name)
-
-install_dotnet_tool("csharp-ls")
-
-envbot.install_package_manager("pipx")
-envbot.install("poetry", "pyright", package_manager="pipx")
-
-envbot.install("pynvim", package_manager="pip")
-envbot.shell("nvim --headless +PlugInstall +qall")
-
-@envbot.step("Prompt install")
-def install_prompt():
-    if envbot.util.is_command_installed("prompt"):
-        raise envbot.StepSkipped()
-
-    envbot.lowlevel.git.git_clone("git@github.com:JamJar00/prompt.git", "~/Projects/prompt")
-    envbot.shell("cd ~/Projects/prompt && cargo build")
-    envbot.ensure_file_contains_text("~/.config/fish/config.fish.local", "fish_add_path ~/Projects/prompt/target/debug")
-
-install_prompt()
-
-# Settings
-if envbot.platform == "Darwin":
-    # Specify the preferences directory
-    envbot.lowlevel.defaults.write("com.googlecode.iterm2.plist", "PrefsCustomFolder", envbot.cwd + "/iterm2")
-    # Tell iTerm2 to use the custom preferences in the directory
-    envbot.lowlevel.defaults.write("com.googlecode.iterm2.plist", "LoadPrefsFromCustomFolder", True)
-
-
-envbot.chsh("fish")
+    envbot.install_package_manager("pipx")
+    envbot.install("poetry", "pyright", package_manager="pipx")
 
 
 envbot.exit()
