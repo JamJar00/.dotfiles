8274554b9f408c61c21129d457f94fcf75efb91d
Jamie Read <jamie.read@and.digital>
2025-09-08 11:49:34 +0100

    Try and improve vim setup in GitPods

--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@
 mkdir -p ~/.config/nvim
 ln -s $(pwd)/vimrc ~/.config/nvim/init.vim || true
 
-if [[ $(uname) == "Linux" ]]; then
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-  sudo rm -rf /opt/nvim
-  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-  rm nvim-linux-x86_64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
-  echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
-fi
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
 
 pip install pynvim
 
@@ -20,3 +18,7 @@ echo 'export GIT_EDITOR="nvim"' >> ~/.bashrc
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+/opt/nvim-linux-x86_64/bin/nvim --headless +PlugInstall +COQdeps +TSUpdate +qall
