d01a075e4711783658e242fef01412d0297d53ff
Jamie Read <jamie.read@and.digital>
2025-09-10 16:04:51 +0100

    fix: install script for new nvim files

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/nvim
-ln -s $(pwd)/nvim ~/.config/nvim || true
+mkdir -p ~/.config
+ln -s $(pwd)/nvim ~/.config/ || true
 
 ln -s $(pwd)/vimrc ~/.vimrc || true
 
 mkdir -p /workspace/.vim
-ln -s /workspace/.vim ~/.vim || true
+ln -s /workspace/.vim ~/.vim/ || true
+
+mkdir -p /workspace/.nvim
+ln -s /workspace/.nvim ~/.local/share/nvim/ || true
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+sudo ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
 
 pip install pynvim
 
@@ -23,7 +26,3 @@ echo 'export GIT_EDITOR="nvim"' >> ~/.bashrc
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-/opt/nvim-linux-x86_64/bin/nvim --headless +PlugInstall +COQdeps +TSUpdate +qall
