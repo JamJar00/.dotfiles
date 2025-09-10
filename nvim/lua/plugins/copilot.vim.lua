01bde65f2bd81f5e67ea9536dd155b1c4b23a571
Jamie Read <hey@jread.dev>
2025-09-10 17:30:12 +0100

    fix: error with copilot when disabled

--- nvim/lua/plugins/copilot.vim.lua
+++ nvim/lua/plugins/copilot.vim.lua
@@ -1,14 +1,11 @@
-if vim.fn.hostname() == "FXJXWHJ0W0.local" then
-  return {
-    'github/copilot.vim',
-    init = function()
-      vim.g.copilot_no_tab_map = true
-    end,
-    config = function()
-      -- Remap copilot to Ctrl-J
-      vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', { expr = true, silent = true, replace_keycodes = false })
-    end
-  }
-else
-  return nil
-end
+return {
+  'github/copilot.vim',
+  enabled = vim.fn.hostname() == "FXJXWHJ0W0.local",
+  init = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+  config = function()
+    -- Remap copilot to Ctrl-J
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', { expr = true, silent = true, replace_keycodes = false })
+  end
+}
