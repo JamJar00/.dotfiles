6bd75277e7f318ada341aa13d6d6253c851a388e
Jamie Read <jamie.read@and.digital>
2025-09-15 15:35:41 +0100

    fix: terminal failing to open second time around

--- nvim/lua/config/terminal.lua
+++ nvim/lua/config/terminal.lua
@@ -1,7 +1,7 @@
 term_buf_nr = -1
 toggle_terminal = function ()
   -- If there's no terminal or the previous one was closed create a new one, else open the old one
-  if term_buf_nr == -1 or not vim.fn.bufexists(term_buf_nr) then
+  if term_buf_nr == -1 or not vim.api.nvim_buf_is_loaded(term_buf_nr) then
     vim.cmd("botright term")
     term_buf_nr = vim.fn.bufnr("$")
   else
