02de8b6d625938af3aa5abf5e3918ee1f4ed737b
Jamie Read <jamie.read@and.digital>
2025-10-01 16:30:17 +0100

    fix: prompt download

--- install.sh
+++ install.sh
@@ -24,7 +24,8 @@ sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
-sudo curl https://github.com/JamJar00/prompt/releases/download/v1.0.1/prompt -o /usr/local/bin/prompt
+sudo curl -L https://github.com/JamJar00/prompt/releases/download/v1.0.1/prompt -o /usr/local/bin/prompt
+sudo chmod +x /usr/local/bin/prompt
 
 echo "export EDITOR=nvim" >> ~/.bashrc
 echo "export GIT_EDITOR=nvim" >> ~/.bashrc
